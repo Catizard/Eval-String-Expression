b3845bdf9547d51b4e46806f0ebf6e3af8ae044f
Catizard <1185032459@qq.com>
2022-10-20 10:28:56 +0800

    complete eval architecture (no test)

--- src/main/java/com/catizard/evalexpr/token/Token.java
+++ src/main/java/com/catizard/evalexpr/token/Token.java
@@ -1,14 +1,14 @@
 package com.catizard.evalexpr.token;
 
 public class Token {
-    public TOKEN_TYPES token_types;
+    public TOKEN_TYPES token_type;
     public String str;
     public int pri;
     public OP_TYPES op_type;
     public enum TOKEN_TYPES {
         TK_NOTYPE /*  blank  */,
         TK_PLUS /*  +  */,
-        TK_MINUS /*  -  */,
+        TK_BINARY_MINUS /*  -  */,
         TK_MULTI /*  *  */,
         TK_DIV /*  /  */,
         TK_LEFT_PARENTHESIS /*  (  */,
@@ -17,11 +17,19 @@ public class Token {
         TK_BINARY_EQ /*  ==  */,
         TK_NEQ /*  !=  */,
         TK_BINARY_AND /*  &&  */,
-        TK_BINARY_OR /* || */
+        TK_BINARY_OR /* || */,
+        TK_UNARY_MINUS /*  -  */,
     }
     public enum OP_TYPES {
         NO_OP,
         UNARY_OP,
         BINARY_OP,
+    }
+
+    public Token(TOKEN_TYPES token_type, String str, int pri, OP_TYPES op_type) {
+        this.token_type = token_type;
+        this.str = str;
+        this.pri = pri;
+        this.op_type = op_type;
     }
 }
--- src/main/java/com/catizard/evalexpr/token/TokenRule.java
+++ src/main/java/com/catizard/evalexpr/token/TokenRule.java
@@ -1,14 +1,44 @@
 package com.catizard.evalexpr.token;
 
+import com.catizard.eval.EvalResult;
+
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
 public class TokenRule {
     private String regex;
     private Token.TOKEN_TYPES token_type;
     private int pri;
     private Token.OP_TYPES op_type;
+    BinaryOperator<EvalResult> binaryOperator;
+    UnaryOperator<EvalResult> unaryOperator;
     public TokenRule(String regex, Token.TOKEN_TYPES token_type, int pri, Token.OP_TYPES op_type) {
         this.regex = regex;
         this.token_type = token_type;
         this.pri = pri;
         this.op_type = op_type;
+    }
+    
+    public TokenRule(String regex, Token.TOKEN_TYPES token_type, int pri, Token.OP_TYPES op_type, BinaryOperator<EvalResult> binaryOperator) {
+        this(regex, token_type, pri, op_type);
+        this.binaryOperator = binaryOperator;
+    }
+
+    public TokenRule(String regex, Token.TOKEN_TYPES token_type, int pri, Token.OP_TYPES op_type, UnaryOperator<EvalResult> unaryOperator) {
+        this(regex, token_type, pri, op_type);
+        this.unaryOperator = unaryOperator;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+    public Token.TOKEN_TYPES getToken_type() {
+        return token_type;
+    }
+    public int getPri() {
+        return pri;
+    }
+    public Token.OP_TYPES getOp_type() {
+        return op_type;
     }
 }
